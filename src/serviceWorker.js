53920226c3ae6f255a7b1968de5119b7caede993
Martijn van Rooijen <534599@student.glu.nl>
2021-06-30 13:46:45 +0200

    h

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -2,9 +2,7 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -29,8 +27,8 @@ export function register(config) {
 
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+            ' ' +
+              ''
           );
         });
       } else {
